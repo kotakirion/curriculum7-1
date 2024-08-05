8823eb21a3063da04099dcfb46ad37119d595dca
kotakirion <rionkotaki0202@keio.jp>
2024-08-05 12:57:14 +0900

    Save current changes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+
         <a href='/posts/create'>create</a>
         <div class='posts'>
             @foreach ($posts as $post)
@@ -15,6 +16,7 @@
                 <h2 class='title'>
                     <a href="/posts/{{ $post->id}}">{{$post->title }}</a>"
                 </h2>
+
                 <p class='body'> {{$post->body}}</p>
              </div>
             @endforeach
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\PostController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
 */
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
